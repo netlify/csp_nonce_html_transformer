90552d53dbb27d0a2e752118773c88a26f7c9a82
jake champion <jake.champion@netlify.com>
2024-11-15 22:40:57 +0000

    compile wasm for deno

--- pkg/csp_nonce_html_transformer.js
+++ pkg/csp_nonce_html_transformer.js
@@ -1,5 +1,3 @@
-let wasm;
-
 const cachedTextDecoder = typeof TextDecoder !== "undefined"
   ? new TextDecoder("utf-8", { ignoreBOM: true, fatal: true })
   : {
@@ -103,18 +101,9 @@ const cachedTextEncoder = typeof TextEncoder !== "undefined"
     },
   };
 
-const encodeString = typeof cachedTextEncoder.encodeInto === "function"
-  ? function (arg, view) {
-    return cachedTextEncoder.encodeInto(arg, view);
-  }
-  : function (arg, view) {
-    const buf = cachedTextEncoder.encode(arg);
-    view.set(buf);
-    return {
-      read: arg.length,
-      written: buf.length,
-    };
-  };
+const encodeString = function (arg, view) {
+  return cachedTextEncoder.encodeInto(arg, view);
+};
 
 function passStringToWasm0(arg, malloc, realloc) {
   if (realloc === undefined) {
@@ -316,182 +305,93 @@ export class HTMLRewriter {
   }
 }
 
-async function __wbg_load(module, imports) {
-  if (typeof Response === "function" && module instanceof Response) {
-    if (typeof WebAssembly.instantiateStreaming === "function") {
-      try {
-        return await WebAssembly.instantiateStreaming(module, imports);
-      } catch (e) {
-        if (module.headers.get("Content-Type") != "application/wasm") {
-          console.warn(
-            "`WebAssembly.instantiateStreaming` failed because your server does not serve Wasm with `application/wasm` MIME type. Falling back to `WebAssembly.instantiate` which is slower. Original error:\n",
-            e,
-          );
-        } else {
-          throw e;
-        }
-      }
-    }
-
-    const bytes = await module.arrayBuffer();
-    return await WebAssembly.instantiate(bytes, imports);
-  } else {
-    const instance = await WebAssembly.instantiate(module, imports);
-
-    if (instance instanceof WebAssembly.Instance) {
-      return { instance, module };
-    } else {
-      return instance;
-    }
-  }
-}
-
-function __wbg_get_imports() {
-  const imports = {};
-  imports.wbg = {};
-  imports.wbg.__wbindgen_string_new = function (arg0, arg1) {
-    const ret = getStringFromWasm0(arg0, arg1);
-    return ret;
-  };
-  imports.wbg.__wbg_element_9f7a29ae173a1783 = function (arg0) {
-    const ret = arg0.element;
-    return isLikeNone(ret) ? 0 : addToExternrefTable0(ret);
-  };
-  imports.wbg.__wbg_element_new = function (arg0) {
-    const ret = Element.__wrap(arg0);
-    return ret;
-  };
-  imports.wbg.__wbg_call_3bfa248576352471 = function () {
-    return handleError(function (arg0, arg1, arg2) {
-      const ret = arg0.call(arg1, arg2);
+const imports = {
+  __wbindgen_placeholder__: {
+    __wbindgen_string_new: function (arg0, arg1) {
+      const ret = getStringFromWasm0(arg0, arg1);
       return ret;
-    }, arguments);
-  };
-  imports.wbg.__wbg_new_9a7e38dd635a4e93 = function (arg0, arg1) {
-    const ret = new TypeError(getStringFromWasm0(arg0, arg1));
-    return ret;
-  };
-  imports.wbg.__wbg_buffer_ccaed51a635d8a2d = function (arg0) {
-    const ret = arg0.buffer;
-    return ret;
-  };
-  imports.wbg.__wbg_newwithbyteoffsetandlength_7e3eb787208af730 = function (
-    arg0,
-    arg1,
-    arg2,
-  ) {
-    const ret = new Uint8Array(arg0, arg1 >>> 0, arg2 >>> 0);
-    return ret;
-  };
-  imports.wbg.__wbg_new_fec2611eb9180f95 = function (arg0) {
-    const ret = new Uint8Array(arg0);
-    return ret;
-  };
-  imports.wbg.__wbindgen_debug_string = function (arg0, arg1) {
-    const ret = debugString(arg1);
-    const ptr1 = passStringToWasm0(
-      ret,
-      wasm.__wbindgen_malloc,
-      wasm.__wbindgen_realloc,
-    );
-    const len1 = WASM_VECTOR_LEN;
-    getDataViewMemory0().setInt32(arg0 + 4 * 1, len1, true);
-    getDataViewMemory0().setInt32(arg0 + 4 * 0, ptr1, true);
-  };
-  imports.wbg.__wbindgen_throw = function (arg0, arg1) {
-    throw new Error(getStringFromWasm0(arg0, arg1));
-  };
-  imports.wbg.__wbindgen_memory = function () {
-    const ret = wasm.memory;
-    return ret;
-  };
-  imports.wbg.__wbindgen_init_externref_table = function () {
-    const table = wasm.__wbindgen_export_2;
-    const offset = table.grow(4);
-    table.set(0, undefined);
-    table.set(offset + 0, undefined);
-    table.set(offset + 1, null);
-    table.set(offset + 2, true);
-    table.set(offset + 3, false);
-  };
-
-  return imports;
-}
-
-function __wbg_init_memory(imports, memory) {
-}
-
-function __wbg_finalize_init(instance, module) {
-  wasm = instance.exports;
-  __wbg_init.__wbindgen_wasm_module = module;
-  cachedDataViewMemory0 = null;
-  cachedUint8ArrayMemory0 = null;
-
-  wasm.__wbindgen_start();
-  return wasm;
-}
-
-function initSync(module) {
-  if (wasm !== undefined) return wasm;
-
-  if (typeof module !== "undefined") {
-    if (Object.getPrototypeOf(module) === Object.prototype) {
-      ({ module } = module);
-    } else {
-      console.warn(
-        "using deprecated parameters for `initSync()`; pass a single object instead",
+    },
+    __wbg_element_9f7a29ae173a1783: function (arg0) {
+      const ret = arg0.element;
+      return isLikeNone(ret) ? 0 : addToExternrefTable0(ret);
+    },
+    __wbg_element_new: function (arg0) {
+      const ret = Element.__wrap(arg0);
+      return ret;
+    },
+    __wbg_call_3bfa248576352471: function () {
+      return handleError(function (arg0, arg1, arg2) {
+        const ret = arg0.call(arg1, arg2);
+        return ret;
+      }, arguments);
+    },
+    __wbg_new_9a7e38dd635a4e93: function (arg0, arg1) {
+      const ret = new TypeError(getStringFromWasm0(arg0, arg1));
+      return ret;
+    },
+    __wbg_buffer_ccaed51a635d8a2d: function (arg0) {
+      const ret = arg0.buffer;
+      return ret;
+    },
+    __wbg_newwithbyteoffsetandlength_7e3eb787208af730: function (
+      arg0,
+      arg1,
+      arg2,
+    ) {
+      const ret = new Uint8Array(arg0, arg1 >>> 0, arg2 >>> 0);
+      return ret;
+    },
+    __wbg_new_fec2611eb9180f95: function (arg0) {
+      const ret = new Uint8Array(arg0);
+      return ret;
+    },
+    __wbindgen_debug_string: function (arg0, arg1) {
+      const ret = debugString(arg1);
+      const ptr1 = passStringToWasm0(
+        ret,
+        wasm.__wbindgen_malloc,
+        wasm.__wbindgen_realloc,
       );
-    }
-  }
+      const len1 = WASM_VECTOR_LEN;
+      getDataViewMemory0().setInt32(arg0 + 4 * 1, len1, true);
+      getDataViewMemory0().setInt32(arg0 + 4 * 0, ptr1, true);
+    },
+    __wbindgen_throw: function (arg0, arg1) {
+      throw new Error(getStringFromWasm0(arg0, arg1));
+    },
+    __wbindgen_memory: function () {
+      const ret = wasm.memory;
+      return ret;
+    },
+    __wbindgen_init_externref_table: function () {
+      const table = wasm.__wbindgen_export_2;
+      const offset = table.grow(4);
+      table.set(0, undefined);
+      table.set(offset + 0, undefined);
+      table.set(offset + 1, null);
+      table.set(offset + 2, true);
+      table.set(offset + 3, false);
+    },
+  },
+};
 
-  const imports = __wbg_get_imports();
-
-  __wbg_init_memory(imports);
-
-  if (!(module instanceof WebAssembly.Module)) {
-    module = new WebAssembly.Module(module);
-  }
-
-  const instance = new WebAssembly.Instance(module, imports);
-
-  return __wbg_finalize_init(instance, module);
+const wasm_url = new URL("csp_nonce_html_transformer_bg.wasm", import.meta.url);
+let wasmCode = "";
+switch (wasm_url.protocol) {
+  case "file:":
+    wasmCode = await Deno.readFile(wasm_url);
+    break;
+  case "https:":
+  case "http:":
+    wasmCode = await (await fetch(wasm_url)).arrayBuffer();
+    break;
+  default:
+    throw new Error(`Unsupported protocol: ${wasm_url.protocol}`);
 }
 
-async function __wbg_init(module_or_path) {
-  if (wasm !== undefined) return wasm;
+const wasmInstance =
+  (await WebAssembly.instantiate(wasmCode, imports)).instance;
+const wasm = wasmInstance.exports;
+export const __wasm = wasm;
 
-  if (typeof module_or_path !== "undefined") {
-    if (Object.getPrototypeOf(module_or_path) === Object.prototype) {
-      ({ module_or_path } = module_or_path);
-    } else {
-      console.warn(
-        "using deprecated parameters for the initialization function; pass a single object instead",
-      );
-    }
-  }
-
-  if (typeof module_or_path === "undefined") {
-    module_or_path = new URL(
-      "csp_nonce_html_transformer_bg.wasm",
-      import.meta.url,
-    );
-  }
-  const imports = __wbg_get_imports();
-
-  if (
-    typeof module_or_path === "string" ||
-    (typeof Request === "function" && module_or_path instanceof Request) ||
-    (typeof URL === "function" && module_or_path instanceof URL)
-  ) {
-    module_or_path = fetch(module_or_path);
-  }
-
-  __wbg_init_memory(imports);
-
-  const { instance, module } = await __wbg_load(await module_or_path, imports);
-
-  return __wbg_finalize_init(instance, module);
-}
-
-export { initSync };
-export default __wbg_init;
+wasm.__wbindgen_start();
--- scripts/build.ts
+++ scripts/build.ts
@@ -1,6 +1,6 @@
 async function main() {
   await Deno.run({
-    cmd: ["wasm-pack", "build", "--target", "web"],
+    cmd: ["wasm-pack", "build", "--target", "deno", "--release"],
   }).status();
   await Deno.remove("./pkg/.gitignore");
 }
