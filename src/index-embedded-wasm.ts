e4e1536c582cf4cfdd7f5cd5cad776e3d8dfc488
jake champion <jake.champion@netlify.com>
2024-11-25 11:36:39 +0000

    feat: add an entrypoint which embeds the wasm instead of requiring it to be fetched over the network when deployed as a Netlify Edge Function
    Netlify Edge Functions are not able to read the file-system, which means we can't read the generated `.wasm` file during execution, we currently fetch the wasm file over the network but this request could fail or have an incorrect response, leading to a runtime error when attempting to instantiate the WebAssembly Module. To work-around this, we when building the `.wasm` file we now also build a `.ts` file which includes the exact same bytes as the wasm file, but within a JavaScipt UInt8Array, which we can then instantiate as a WebAssembly Module.

--- src/index-embedded-wasm.ts
+++ src/index-embedded-wasm.ts
@@ -1,4 +1,5 @@
 import { default as init } from "../pkg/csp_nonce_html_transformer.js";
-await init();
+import { wasmBinary } from "../pkg/embedded-wasm.ts";
+await init(wasmBinary);
 
 export { csp } from "./csp.ts";
---
+++ tests/index-embedded-wasm.test.ts
@@ -0,0 +1,5 @@
+import { csp } from "../src/index-embedded-wasm.ts";
+
+import tests from "./tests.ts";
+
+tests(csp);
