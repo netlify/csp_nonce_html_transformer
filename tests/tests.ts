8830d86364966c9cb906d71f47d5ab4ca7735efd
Jake Champion <jake.champion@netlify.com>
2025-07-18 22:14:59 +0100

    Update tests.ts

--- tests/tests.ts
+++ tests/tests.ts
@@ -3,9 +3,9 @@ import {
   assertStrictEquals,
 } from "https://deno.land/std@0.224.0/assert/mod.ts";
 
-import * as cheerio from "https://cdn.jsdelivr.net/npm/cheerio@1.1.0/dist/browser/index.js";
+import * as cheerio from "https://esm.sh/cheerio@1.1.0/dist/browser/index.js";
 
-import cspParser from "https://cdn.jsdelivr.net/npm/content-security-policy-parser@0.6.0/script/mod.js/+esm";
+import cspParser from "https://esm.sh/content-security-policy-parser@0.6.0/script/mod.js";
 import { assertEquals } from "https://deno.land/std@0.224.0/assert/assert_equals.ts";
 import { assert } from "https://deno.land/std@0.224.0/assert/assert.ts";
 import { assertArrayIncludes } from "https://deno.land/std@0.224.0/assert/assert_array_includes.ts";
