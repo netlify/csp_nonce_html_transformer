726b51625a13863a8d62abb595b17f0049ee9695
Jake Champion <jake.champion@netlify.com>
2025-07-18 22:11:55 +0100

    use sri safe url

--- tests/tests.ts
+++ tests/tests.ts
@@ -3,7 +3,7 @@ import {
   assertStrictEquals,
 } from "https://deno.land/std@0.224.0/assert/mod.ts";
 
-import * as cheerio from "https://cdn.jsdelivr.net/npm/cheerio/+esm";
+import * as cheerio from "https://cdn.jsdelivr.net/npm/cheerio@1.1.0/dist/browser/index.js";
 
 import cspParser from "https://cdn.jsdelivr.net/npm/content-security-policy-parser@0.6.0/script/mod.js/+esm";
 import { assertEquals } from "https://deno.land/std@0.224.0/assert/assert_equals.ts";
